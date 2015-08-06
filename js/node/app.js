267602cff0832553e41d7ae475a41c3fbe0a6ae9
piercefrawley <pierce@procore.com>
2015-08-06 01:04:41 -0700

    Clean Up

--- js/node/app.js
+++ js/node/app.js
@@ -1,14 +1,15 @@
-const http = require('http');
-const PORT=8080; 
-const app = require('express')();
-const server = http.createServer(app);
-const io = require('socket.io')(server);
+const app     = require('express')();
+const http    = require('http');
+const server  = http.createServer(app);
+const io      = require('socket.io')(server);
+const PORT    = 8080; 
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/', function(request, response) {
+  response.send('GET homepage request');
 });
 
 io.on('connection', function(){ /* … */ });
+
 server.listen(PORT, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
 });
