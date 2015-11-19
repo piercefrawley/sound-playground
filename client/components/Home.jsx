3351be9c1a5524e9390bccecf8c14f3be4a3d004
piercefrawley <pierce@procore.com>
2015-11-18 21:11:51 -0800

    Nav Bar Adjustment

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,20 +4,16 @@ const { Link } = ReactRouter;
 Home = class Home extends React.Component {
   render() {
     return (
-      <div className='foo'>
-        <div>
-          <h1>Sound Playground</h1>
-        </div>
-        <div>
-          <p>This is the home route.</p>
-          <Link to='/login'>Login Page</Link>
-        </div>
-        <div>
-          <MidiSynth/>
-          <div className="btn-group btn-group-lg" role="group" aria-label="...">
-            <button type="button" className="btn btn-default">Left</button>
-            <button type="button" className="btn btn-default">Middle</button>
-            <button type="button" className="btn btn-default">Right</button>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="page-header">
+            <h1>Sound Playground</h1>
+            <p className="navbar-text navbar-right">
+              <Link to='/login'>Login Page</Link>
+            </p>
+          </div>
+          <div>
+            <MidiSynth/>
           </div>
         </div>
       </div>
