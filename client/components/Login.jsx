9b7b57f8b14d734180c504119426768f10c0d410
piercefrawley <pierce@procore.com>
2015-12-01 19:50:20 -0800

    Add Login With Accounts

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -2,61 +2,77 @@ const { Link } = ReactRouter;
 
 // Login Component
 Login = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    return {
+      users: Meteor.users,
+      currentUser: Meteor.user()
+    };
+  },
   render() {
     return (
       <div className='row'>
         <h1 className='center-block'>Welcome to the Login Page</h1>
-        <div className='col-md-4'>
-          <form id="sign-in-with-email" onSubmit={this.onSubmit}>
+        <div className='col-md-8'>
+          <form onSubmit={this._signin}>
             <div className="form-group">
-              <label for="emailAddress">Email Address</label>
+              <label>Email Address</label>
               <input type="email" name="email" className="form-control" placeholder="Enter Email"/>
             </div>
             <div className="form-group">
-              <label for="password">Password</label>
+              <label>Password</label>
               <input type="password" name="password" className="form-control" placeholder="Enter Password"/>
             </div>
             <div className="form-footer">
-              <button type="submit" refs='create' className="btn btn-primary btn-create-account">
+              <button className="btn btn-primary btn-create-account">
                 Create Account
               </button>
-              <button type="submit" refs='signin' className="btn btn-default btn-sign-in">
+              <button className="btn btn-default btn-sign-in">
                 Sign In
               </button>
             </div>
           </form>
         </div>
         <LoginOptions/>
+        <button className="btn btn-default btn-logout" onClick={this._logout}>
+          Logout
+        </button>
       </div>
     );
   },
 
-  onSubmit(event) {
-    const email = $(event.target).find("[name=email]").val();
-    const password = $(event.target).find("[name=password]").val();
+  _signin(e) {
+    const email = $(e.target).find("[name=email]").val();
+    const password = $(e.target).find("[name=password]").val();
     const errors = {};
 
-    if (!email) {
-      errors.email = "Email required"
-    }
-
-    if (!password) {
-      errors.password = "Password required"
-    }
-
+    if (!email) { errors.email = "Email required" }
+    if (!password) { errors.password = "Password required"}
     this.setState({ errors });
 
     if(!R.isEmpty(errors)) { return; }
 
-    Meteor.loginWithPassword(email, password, (err) => {
-      if (err) {
-        this.setState({
-          errors: {'none': err.reason}
-        });
-        return;
-      } else {
-        ReactRouter.history.pushState(null, `/`);
+    Accounts.createUser({
+      email : email,
+      password : password,
+      profile  : {
+        slogan: 'booyahhh'
       }
     });
+    // Accounts.loginWithPassword(email, password, (err) => {
+    //   if (err) {
+    //     console.log(err.reason);
+    //     return;
+    //   } else {
+    //     debugger;
+    //     ReactRouter.history.pushState(null, `/`);
+    //   }
+    // });
+  },
+
+  _logout() {
+    e.preventDefault();
+    Accounts.user && Meteor.logout();
   }
+
 });
